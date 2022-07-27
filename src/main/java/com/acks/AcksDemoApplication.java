1c6564f243128df79169242d64c3f4cf0a34e992
Chetan Kabra <chetankabra14th@gmail.com>
2022-07-27 09:02:48 +0530

    Project Uploaded

---
+++ src/main/java/com/acks/AcksDemoApplication.java
@@ -0,0 +1,13 @@
+package com.acks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcksDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcksDemoApplication.class, args);
+	}
+
+}
